79c46d38860f554da106fd2f797339c1029d5fbd
Rachel Buchta <rachelbuchta@yahoo.com>
2021-02-04 19:32:17 -0700

    Add styling to movie detail card

--- src/Components/MovieDetails/MovieDetails.js
+++ src/Components/MovieDetails/MovieDetails.js
@@ -1,9 +1,24 @@
 import React from "react"
+import './MovieDetails.scss'
 
 const MovieDetails = ({currentMovie, handleClick}) => {
   return (
-
-    <h1>{currentMovie.title}</h1>
+    <main>
+      <section className="detailsContainer">
+        <div className="txtContainer">
+          <h1>{currentMovie.title}</h1>
+          <p>This is a movie its probably good but idk its about a 3 on a 5 scale/ Got some characters, its gots a plot, its got an ending.Boom.</p>
+          <div className="dateContainer">
+            <p>{currentMovie.release_date}</p>
+            <p>{currentMovie.average_rating}</p>
+          </div>
+          <p>Genre: RomCOM</p>
+          <p>Revenue: $1,000,000,000</p>
+          <p>Budget: $40</p>
+        </div>
+        <img src={currentMovie.poster_path} alt=""/>
+      </section>
+    </main>
   )
 }
 
