78ccb49ccd24df66db7d0f4c0747d9121bbc4714
Rachel Buchta <rachelbuchta@yahoo.com>
2021-02-08 16:05:00 -0700

    Test when a single movie is clicked shows details

--- cypress/integration/movie_reelz.js
+++ cypress/integration/movie_reelz.js
@@ -4,6 +4,19 @@ beforeEach(() => {
 
 describe('Movie Reelz', () => {
   it('Should be able to visit the page and render all movies', () => {
-      cy.get('.moviesContainer').children('.movieCard');
+    cy.get('.moviesContainer').children('.movieCard');
+  })
+})
+
+describe('Movie Reelz', () => {
+  it('Should be able to click a single movie, all movies disapear and are navigated to a new page that displays that single movies details and trailer', () => {
+    cy.get('.movieCard:first').click()
+  })
+})
+
+describe('Movie Reelz', () => {
+  it('Should display a section containing movie details, a trailer and a backdrop image related to that movie', () => {
+    cy.get('.movieCard:first').click()
+    cy.get('main').children('.backdrop', '.detailsWrapper')
   })
 })
