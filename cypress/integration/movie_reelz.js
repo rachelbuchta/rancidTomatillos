c649cd2d20c454bad1dfb5623082bf992a6d4a3e
Rachel Buchta <rachelbuchta@yahoo.com>
2021-02-08 18:54:57 -0700

    Finish writing tests for current functionality

--- cypress/integration/movie_reelz.js
+++ cypress/integration/movie_reelz.js
@@ -5,18 +5,25 @@ beforeEach(() => {
 describe('Movie Reelz', () => {
   it('Should be able to visit the page and render all movies', () => {
     cy.get('.moviesContainer').children('.movieCard');
+    //check url
   })
-})
 
-describe('Movie Reelz', () => {
   it('Should be able to click a single movie, all movies disapear and are navigated to a new page that displays that single movies details and trailer', () => {
     cy.get('.movieCard:first').click()
+    //check url
   })
-})
 
-describe('Movie Reelz', () => {
   it('Should display a section containing movie details, a trailer and a backdrop image related to that movie', () => {
     cy.get('.movieCard:first').click()
     cy.get('main').children('.backdrop', '.detailsWrapper')
+    cy.get('.videoContainer').children('iframe')
+    
+  })
+
+  it('Should return to home page view showing all movies when the exit button is clicked', () => {
+    cy.get('.movieCard:first').click()
+    cy.get('.detailsContainer').children('.btnWrapper').children('button').click()
+    //check url
   })
 })
+
