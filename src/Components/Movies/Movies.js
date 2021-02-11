712426509c5a3d8130e58e5b6f2bf56ac61f21cd
Rachel Buchta <rachelbuchta@yahoo.com>
2021-02-10 22:52:32 -0700

    Add functionality that renders the amount of stars

--- src/Components/Movies/Movies.js
+++ src/Components/Movies/Movies.js
@@ -3,12 +3,16 @@ import './Movies.scss'
 import Movie from '../Movie/Movie'
 
 const Movies = ({movies, getSingleMovieData}) => {
+
   const movieCards = movies.map(movie => {
     return (
       <Movie 
         image={movie.poster_path}
+        title={movie.title}
+        average_rating={movie.average_rating}
         id={movie.id}
         key={movie.id}
+        // starCount={starCount}
         getSingleMovieData={getSingleMovieData}
       />
     )
