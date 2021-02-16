2393bfb2b3c67067d1604c44012d5d14ef999ce4
Rachel Buchta <rachelbuchta@yahoo.com>
2021-02-16 16:46:29 -0700

    Lint files again

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,20 +1,19 @@
 import React from 'react'
 import './Footer.scss'
-import { Link } from 'react-router-dom'
 
 const Footer = () => {
-    return (
-        <footer>
-            <div className='footerMotto'>
-                <p>Get Real About Your Reelz</p>
-            </div>
-            <div className='linkedIn'>
-                <p>Contact Us:</p>
-                <a class='linkedInProfile' href='https://www.linkedin.com/in/colefiscus/'>Cole Fiscus</a>
-                <a class='linkedInProfile' href='https://www.linkedin.com/in/rachelbuchta/'>Rachel Buchta</a>
-            </div>
-        </footer>
-    )
+  return (
+    <footer>
+      <div className='footerMotto'>
+        <p>Get Real About Your Reelz</p>
+      </div>
+      <div className='linkedIn'>
+        <p>Contact Us:</p>
+        <a className='linkedInProfile' href='https://www.linkedin.com/in/colefiscus/'>Cole Fiscus</a>
+        <a className='linkedInProfile' href='https://www.linkedin.com/in/rachelbuchta/'>Rachel Buchta</a>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
